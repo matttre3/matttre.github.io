bc2ca98a9db2067d90c525b129202b892090fdc8
matttre3 <140153086+matttre3@users.noreply.github.com>
2023-08-25 12:13:46 +0200

    Update

---
+++ index.js
@@ -0,0 +1,40 @@
+const typewriterElement = document.getElementById("typewriter");
+const words = ["UX/UI Designer", "Front-end Developer", "Digital Marketer"];
+let wordIndex = 0;
+let charIndex = 0;
+let isDeleting = false;
+
+function type() {
+    const currentWord = words[wordIndex];
+    const text = isDeleting ? currentWord.substring(0, charIndex - 1) : currentWord.substring(0, charIndex + 1);
+
+    typewriterElement.textContent = text;
+
+    if (!isDeleting) {
+        charIndex++;
+    } else {
+        charIndex--;
+    }
+
+    if (charIndex === currentWord.length + 1) {
+        isDeleting = true;
+    }
+
+    if (charIndex === 0 && isDeleting) {
+        isDeleting = false;
+        wordIndex = (wordIndex + 1) % words.length;
+    }
+
+    const typingSpeed = isDeleting ? 100 : 150; // Velocità di scrittura e cancellazione
+    setTimeout(type, typingSpeed);
+}
+
+type(); // Avvia la funzione di scrittura
+
+
+function closeMenu() {
+    let menu = document.querySelector('.navbar-collapse');
+    if (menu.classList.contains('show')) {
+      menu.classList.remove('show');
+    }
+  }
